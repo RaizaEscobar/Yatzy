f98287963e92db42cf57e8f4d952da4a39a15ab4
Raiza Escobar <raizaescobar@gmail.com>
2020-09-13 19:40:50 +0200

    cambios

--- yatzy.js
+++ yatzy.js
@@ -1,3 +1,16 @@
 function throwDieces(){
-    console.log("Hola raiza!");
+    const dieces = document.getElementsByClassName("diece");
+    for (let i = 0; i < dieces.length; i++) {
+        dieces[i].src = generateRandomPath();    
+    }
 }
+
+function generateRandomPath(){
+    let random = Math.floor(Math.random() * 6) + 1;         
+    return `Img/dado${random}.png`;  
+}
+
+function selectDiece(img){
+    img.className += " selected";
+}
+
